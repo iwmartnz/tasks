c8fc7e8dd844fa178260f1f94e999080adea70fc
Isra <iwmartnz@gmail.com>
2024-01-22 13:36:57 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<>
+			<h1 className=" text-4xl font-medium md:text-5xl">Tasks</h1>
+		</>
+	)
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+export default {
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['"Urbanist Variable"', ...defaultTheme.fontFamily.sans]
+			},
+			colors: {
+				'brand-black': '#141414',
+				'brand-dark-gray': '#242424',
+				'brand-gray': '#343434',
+				'brand-light-gray': '#545454',
+				'brand-white': '#f5f5f5',
+				'brand-accent': '#64f5b7'
+			}
+		}
+	},
+	plugins: []
+}
